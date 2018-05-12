ab9c8a166d307402107665d50a99a693c17cce48
Vpugh <taws26@gmail.com>
2018-05-12 00:46:53 -0400

    Update production script

--- config/index.js
+++ config/index.js
@@ -43,7 +43,8 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    // assetsPublicPath: '/',
+    assetsPublicPath: '/https://vpugh.github.io/vue-pomodoro-timer/',
 
     /**
      * Source Maps
